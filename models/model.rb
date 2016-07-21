b5beb86bfb1cd48834d210cc1d428abbf4110a5b
Nicole Cordero <nicolec1@tywls-astoria.org>
2016-07-21 18:13:52 +0000

    model changed

--- models/model.rb
+++ models/model.rb
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-def describe(adj="actual human sunshine")
-  "Karlie is #{adj}"
-=======
-=======
->>>>>>> e799990e49e6a97a64745db1a6a30c43bcd58067
 def evaluate(symptom)
   @symptoms_hash = {
-    "nausea" => ["nausea med1", "nausea med2"],
-    "headaches" => ["headaches med1", "headaches med2"],
-    "fever" => ["fever med1", "fever med2"]
+    "Weak/Uneasy" => ["Tylenol", "Advil", "Mucinex"],
+    "Dizzy/Light Headed" => ["Excedrin", "Tylenol", "Motrin"],
+    "Uncomfortable/Indifferent to foods." => ["Emetrol", "pre-vacid", "Nexium"]
   }
-<<<<<<< HEAD
-  params[:symptom]
->>>>>>> 74102782eac951655554ef7c886d34946848288e
-=======
+
   @symptoms_hash[symptom]
->>>>>>> e799990e49e6a97a64745db1a6a30c43bcd58067
 end

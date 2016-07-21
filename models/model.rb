197848a792b2408249a908204add7adb48f897cf
Elysha Ang <elysha_ang@aol.com>
2016-07-21 17:52:52 +0000

    big change

--- models/model.rb
+++ models/model.rb
@@ -1,8 +1,9 @@
 def evaluate(symptom)
   @symptoms_hash = {
-    "nausea" => ["nausea med1", "nausea med2"],
-    "headaches" => ["headaches med1", "headaches med2"],
-    "fever" => ["fever med1", "fever med2"]
+    "Weak/Uneasy" => ["Tylenol", "Advil", "Mucinex"],
+    "Dizzy/Light Headed" => ["Excedrin", "Tylenol", "Motrin"],
+    "Uncomfortable/Indifferent to foods." => ["Emetrol", "pre-vacid", "Nexium"]
   }
+
   @symptoms_hash[symptom]
 end
